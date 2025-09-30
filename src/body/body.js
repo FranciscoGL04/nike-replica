93074ebfcd2554f0bb794a542a7b5ee3e00ee15b
Francisco <franciscoeglopez@gmail.com>
2025-09-30 10:21:14 +0200

    changed video, made it responsive and added top bar

--- src/body/body.js
+++ src/body/body.js
@@ -1,93 +1,90 @@
 import "../body/css/style.css";
+import { Link } from "react-router-dom";
 import fiftyOff from "./images/fiftyOff.png";
 import men from "./images/men.png";
 import women from "./images/women.png";
 import kids from "./images/kids.png";
-
-import Sale from "../sale/sale";
-import Men from "../men/men";
+import video from "./images/video1280version.mp4";
 
 function Body() {
   return (
     <div>
-      <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <iframe
-              width="1500"
-              height="720"
-              src="https://www.youtube.com/embed/UM6OeVkZAd4?si=&autoplay=1&mute=1&controls=0&loop=1&playlist=UM6OeVkZAd4"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-            ></iframe>
-          </div>
+      {/* Hero Video Section */}
+      <div className="hero-section">
+        <div className="video-container">
+          <video
+            src={video} 
+            className="hero-video"
+            autoPlay
+            muted
+            loop
+            playsInline
+          />
+        </div>
+        <div className="hero-text">
+          <h4 className="hero-subtitle">
+            The can-do self is 42.2 km ahead of you.
+          </h4>
+          <h1 className="hero-title">JUST DO IT.</h1>
         </div>
       </div>
-      <div class="carousel-text">
-        <h4 id="h4-text">
-          The you that you know you can is 42.2 kilometers away.
-        </h4>
-        <h1 id="h1-text">JUST DO IT.</h1>
-      </div>
 
-      <div class="container text-center">
-        <div class="row row-cols-2">
-          <div class="col">
-            <div class="card w-75 mb-3">
-              <div class="card-body">
-                <a href="/sale">
-                  <img class="cardsImages" src={fiftyOff} />
-                </a>
-                <p class="card-text">Check Our Articles On Sale!</p>
-                <h5 class="card-title">Up to 50% Off!</h5>
-                <a href="/sale" class="btn btn-primary">
-                  Shop
-                </a>
-              </div>
+      {/* Cards Section */}
+      <div className="container">
+        <div className="cards-grid">
+          {/* Sale Card */}
+          <div className="card-wrapper">
+            <div className="product-card">
+              <Link to="/sale" className="card-link">
+                <img className="card-image" src={fiftyOff} alt="Sale" />
+                <div className="card-overlay">
+                  <p className="card-text">Check Our Articles On Sale!</p>
+                  <h5 className="card-title">Up to 50% Off!</h5>
+                  <button className="card-btn">Shop</button>
+                </div>
+              </Link>
             </div>
           </div>
-          <div class="col">
-            <div class="card w-75 mb-3">
-              <div class="card-body">
-                <a href="/men">
-                  <img class="cardsImages" src={men} />
-                </a>
-                <p class="card-text">Our Picks For Men</p>
-                <h5 class="card-title">Men's Collection</h5>
-                <a href="/men" class="btn btn-primary">
-                  Shop
-                </a>
-              </div>
+
+          {/* Men Card */}
+          <div className="card-wrapper">
+            <div className="product-card">
+              <Link to="/men" className="card-link">
+                <img className="card-image" src={men} alt="Men's Collection" />
+                <div className="card-overlay">
+                  <p className="card-text">Our Picks For Men</p>
+                  <h5 className="card-title">Men's Collection</h5>
+                  <button className="card-btn">Shop</button>
+                </div>
+              </Link>
             </div>
           </div>
-          <div class="col">
-            <div class="card w-75 mb-3">
-              <div class="card-body">
-                <a href="/women">
-                <img class="cardsImages" src={women} />
-                </a>
-                <p class="card-text">Women's Top Picks Here</p>
-                <h5 class="card-title">Women's Collection</h5>
-                <a href="#" class="btn btn-primary">
-                  Shop
-                </a>
-              </div>
+
+          {/* Women Card */}
+          <div className="card-wrapper">
+            <div className="product-card">
+              <Link to="/women" className="card-link">
+                <img className="card-image" src={women} alt="Women's Collection" />
+                <div className="card-overlay">
+                  <p className="card-text">Women's Top Picks Here</p>
+                  <h5 className="card-title">Women's Collection</h5>
+                  <button className="card-btn">Shop</button>
+                </div>
+              </Link>
             </div>
           </div>
-          <div class="col">
-            <div class="card w-75 mb-3">
-              <div class="card-body">
-                <a href="/kids">
-                <img class="cardsImages" src={kids} />
-                </a>
-                <p class="card-text">The Best For Our Kids</p>
-                <h5 class="card-title">Kids Collection</h5>
-                <a href="#" class="btn btn-primary">
-                  Shop
-                </a>
-              </div>
+
+          {/* Kids Card */}
+          <div className="card-wrapper">
+            <div className="product-card">
+              <Link to="/kids" className="card-link">
+                <img className="card-image" src={kids} alt="Kids Collection" />
+                <div className="card-overlay">
+                  <p className="card-text">The Best For Our Kids</p>
+                  <h5 className="card-title">Kids Collection</h5>
+                  <button className="card-btn">Shop</button>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
--- src/header/header.js
+++ src/header/header.js
@@ -2,46 +2,70 @@ import "../header/css/style.css"
 import { Link } from "react-router-dom";
 import heart from "../assets/heart.png";
 import shoppingbag from "../assets/shopping-bag.png"
+import logo from "./logo/jordanLogo.png";
 
 function Header() {
     return(
-      <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-      <Link class="navbar-brand" to="/">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" alt="Nike Logo" id="logo"/>
-      </Link>
-
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item"><Link class="nav-link" to="/new">New</Link></li>
-          <li class="nav-item"><Link class="nav-link" to="/men">Men</Link></li>
-          <li class="nav-item"><Link class="nav-link" to="/women">Women</Link></li>
-          <li class="nav-item"><Link class="nav-link" to="/kids">Kids</Link></li>
-          <li class="nav-item"><Link class="nav-link" to="/sport">Sport</Link></li>
-          <li class="nav-item"><Link class="nav-link" to="/sale">Sale</Link></li>
-        </ul>
-
-        <div class="d-flex align-items-center ms-auto">
-          <div class="search-box me-3">
-            <input type="text" placeholder="Search"/>
-            <i class="fas fa-search"></i>
-          </div>
-          <div class="nav-icons">
-            <i>
-            <a><img src={heart} alt="heartIcon" class="icons"/></a>
-            </i>
-            <i class="fas fa-shopping-bag">
-            <a><img src={shoppingbag} alt="heartIcon" class="icons"/></a>
-            </i>
-          </div>
+      <div>
+        <div id="topBar">
+        <div id="topBarLogo">
+            <Link>
+            <img src={logo} 
+            alt="jordanLogo"
+            id="jordanLogo"
+            />
+            </Link>
         </div>
+        <div id="topBarList">
+            <ul id="topList">
+              <li class="listItem"><Link class="listLink">Search store</Link></li>|
+              <li class="listItem"><Link class="listLink">Help</Link></li>|
+              <li class="listItem"><Link class="listLink">Be there</Link></li>|
+              <li class="listItem"><Link class="listLink">Register</Link></li>
+            </ul>
+        </div>
+        </div>
+        
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" alt="Nike Logo" id="logo"/>
+          </Link>
+
+          {/* Desktop Navigation */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item"><Link className="nav-link" to="/new">New & Featured</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/men">Men</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/women">Women</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/kids">Kids</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/sale">Sale</Link></li>
+            </ul>
+          </div>
+
+          {/* Search and Icons */}
+          <div className="d-flex align-items-center ms-auto">
+            <div className="search-box me-3">
+              <i className="fas fa-search"></i>
+              <input type="text" placeholder="Search"/>
+            </div>
+            <div className="nav-icons">
+              <Link to="/favorites">
+                <img src={heart} alt="Favorites" className="icons"/>
+              </Link>
+              <Link to="/cart">
+                <img src={shoppingbag} alt="Shopping Bag" className="icons"/>
+              </Link>
+            </div>
+          </div>
+
+          {/* Mobile Toggle Button */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
       </div>
-    </div>
-  </nav>
     ) 
 }
 
